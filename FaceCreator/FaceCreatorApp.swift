882425241b7900cec0d2aab286a4138f8ee03f73
Vincent He <vincent0710@Outlook.com>
2020-09-27 16:55:30 -0400

    Initial Commit

---
+++ FaceCreator/FaceCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceCreatorApp.swift
+//  FaceCreator
+//
+//  Created by Vincent He on 2020-09-27.
+//
+
+import SwiftUI
+
+@main
+struct FaceCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
